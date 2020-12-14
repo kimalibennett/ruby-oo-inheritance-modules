0a8a8d8ebe1c90db0188a1eef432cd5482ca56bb
kimalibennett <aminbennett@gmail.com>
2020-12-14 15:22:19 -0600

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,11 +1,12 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
-def Kid 
+class Kid
     include Dance
-
+    extend MetaDancing
+  
     attr_accessor :name
-
+  
     def initialize(name)
-        @name = name
+      @name = name
     end
-end
+  end 
